4fe882bc936a8c85897c435b0637263826b66313
ayushbirla71 <ayushbirla71@gmail.com>
2023-02-02 23:54:05 +0530

    edit

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const mongoose= require('mongoose')
 const route = require('./routes/route')
+const serverless = require("serverless-http");
 
 const cors=require("cors");
 const corsOptions ={
@@ -19,3 +20,6 @@ mongoose.connect("mongodb+srv://ayush8120:GeGo5qhr7wM6VQyg@cluster0.n1nevi5.mong
 app.use(`/`,route)
 app.listen(process.env.PORT||3001, function(){console.log('Our First Project running On PORT '+(process.env.PORT||3001))
 })
+
+module.exports = app;
+module.exports.handler = serverless(app);
